8c1355f874242056ef1f19c2f762c6561ce83cd5
Luzefiru <ian.deejay.iv@gmail.com>
2024-01-09 00:27:25 +0800

    fix: change payload to form-urlencoded to match docs

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const path = require('path');
 /* Middleware */
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 require('express-async-errors');
 app.set('views', path.join(__dirname, './src/views'));
 app.set('view engine', 'ejs');
