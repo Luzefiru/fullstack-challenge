ac00f91b0c7120d96e3fa88603d97834d20edf39
Luzefiru <ian.deejay.iv@gmail.com>
2024-01-09 02:27:57 +0800

    feat: create function to build tree structure

--- server/src/controllers/home.controller.js
+++ server/src/controllers/home.controller.js
@@ -3,7 +3,36 @@ const axios = require('axios');
 
 const getTerritoryData = async () => {
   const { data } = await axios.get(config.TERRITORY_DATA_URL);
-  return data;
+
+  const buildHierarchy = (root, parentId = null) => {
+    const children = root.filter((item) => item.parent === parentId);
+
+    if (children.length === 0) {
+      return null;
+    }
+
+    const tree = children.map((child) => {
+      const subtree = buildHierarchy(root, child.id);
+      if (subtree) {
+        return { ...child, children: subtree };
+      }
+      return { ...child };
+    });
+
+    return tree;
+  };
+
+  const generateHierarchy = (data) => {
+    const rootNodes = data.filter((item) => !item.parent);
+    const hierarchicalData = rootNodes.map((root) => {
+      const subtree = buildHierarchy(data, root.id);
+      return { ...root, children: subtree };
+    });
+
+    return hierarchicalData;
+  };
+
+  return generateHierarchy(data.data);
 };
 
 module.exports = { getTerritoryData };
